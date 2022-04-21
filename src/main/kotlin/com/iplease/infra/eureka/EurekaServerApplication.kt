3aa953d55525caaa9ca7c37a72801b77f1318fd8
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-04-21 14:00:00 +0900

    [ADD] Dockerfile 정의 및 EurekaServer 활성화

--- src/main/kotlin/com/iplease/infra/eureka/EurekaServerApplication.kt
+++ src/main/kotlin/com/iplease/infra/eureka/EurekaServerApplication.kt
@@ -2,7 +2,9 @@ package com.iplease.infra.eureka
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
+@EnableEurekaServer
 @SpringBootApplication
 class EurekaServerApplication
 
